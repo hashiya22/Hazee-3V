261d3a109e15ff9380366b62a92c056166509f6a
hashiya22 <githubhashindu@gmail.com>
2025-12-24 02:21:36 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aRw1gRyJ#T4g_ETJ5exaR7yuC0s9l8Yob3ZhVuODgulZUgUC-4vk", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "HcAiwDrK#aJd2SWV0gBNOzY9t9tG_jXZEf8FDLY2VtV9Bf9TQAX0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94771163079",
